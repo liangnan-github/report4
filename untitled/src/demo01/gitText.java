4f6ac3b7521ab13105ec1e1bf8bd5bfea760b008
root <itcast>
2019-03-11 11:22:22 +0800

    又添加几句話

---
+++ untitled/src/demo01/gitText.java
@@ -0,0 +1,14 @@
+package demo01;
+
+public class gitText {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+        System.out.println("helloworld");
+    }
+
+}
